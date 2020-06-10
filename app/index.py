4fa37016c2b004c81903dd562db89b7e2d91ddc0
udoyen <datameshprojects@gmail.com>
2020-06-10 14:35:00 +0100

    removed newline characters from dict outputs

--- app/index.py
+++ app/index.py
@@ -1,9 +1,56 @@
-import sys
-import subprocess
 import os
-from shlex import quote as shlex_quote
+import subprocess
+import re
+from collections import OrderedDict
+import json
 
-if __name__ == '__main__':
+# regex code
+"""
+email: ^Hello\sWorld,.*and\s(.*@\w+\.\w+)\.{0,1}$
+names: ^Hello\s*World,\s.+is\s*([\w\s-]*)\swith.*
+hng id: ^Hello\s*World,\s.+ID\s(HNG-\d{5})
+language: ^Hello\s*World,\s.+using\s(\w+)
+email = re.match(r"^Hello\sWorld,.*and\s(.*@\w+\.\w+)\.{0,1}$", email)
+name = re.match(r"^Hello\s*World,\s.+is\s([\w\s]+)\swith", email)
+test = re.match(r"", sample)
+print(test.group(1)) if type(test) else print('Failed')
+"""
+
+email = "^.+and\s+([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+).*$"
+names = "^Hello\s*World,\s.+is\s*([\w\s-]*)\swith.*"
+hng_id = "^Hello\s*World,\s.+ID\s(HNG-\d{5})"
+language = "^Hello\s*World,\s.+using\s(\w+)"
+output = "^Hello\sWorld,\s+this\s+is\s+[\w\s-]+with\s+HNGi7\s+ID\s+HNG-\d{5}\s+using\s+\w+\s+for\s+stage\s+2\s+task\s+and\s+[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[\.|\s]*$"
+
+# sample json output
+json_data = []
+
+# return value from function
+output_results = OrderedDict()
+
+
+def check_info(test_regex: str, user_output: str) -> str:
+    """
+    Used to confirm user information
+    :param test_regex:
+    :param user_output:
+    :return (str): string of user information or empty string
+    """
+    result = re.match(test_regex, user_output)
+    if result:
+        if test_regex == output:
+            return result.group()
+        else:
+            return result.group(1)
+    else:
+        return ''
+
+
+def process_users() -> list:
+    """
+    Used to process user scripts
+    :return (list): Tuple of list of failures and dict of all data
+    """
     run_command = {
         '.js': 'node',
         '.py': 'python3',
@@ -16,32 +63,68 @@ if __name__ == '__main__':
         '.java': 0,
         '.py': 0
     }
-    error_file = []
-    target = os.path.abspath('scripts')
-    print(target)
+    error_files = []
+    dir_name = os.path.dirname(__file__)
+    r_path = os.path.realpath(dir_name + '/scripts')
+    target = r_path
+    # TODO remove these lines
+    print('Dirname: {}'.format(dir_name))
+    print('Location: {}'.format(r_path))
+    print('Target: {}'.format(target))
     try:
         # Get files in scripts folder
         files = [f for f in os.listdir(target) if os.path.isfile(os.path.join(target, f))]
         for f in files:
             # Get file extension
             file_extension = os.path.splitext(f)
-            print('====================================')
-            print('File extension: {}'.format(str(file_extension[1])))
-            print('File: {}'.format(f))
-            print('Fullpath to file: {}'.format(os.path.abspath('scripts/' + f)))
-            f_path = os.path.abspath('scripts/' + f)
+            # TODO remove these lines
+            # print('====================================')
+            # print('File extension: {}'.format(str(file_extension[1])))
+            # print('File: {}'.format(f))
+            # print('Fullpath to file: {}'.format(os.path.abspath('scripts/' + f)))
+            f_path = r_path + '/' + f
             if file_extension[1] in run_command.keys():
                 # Update the language count dict
                 lang_count[file_extension[1]] += 1
                 # Run the command and get the output
-                print('Command: {}'.format(run_command[file_extension[1]]))
+                # TODO Remove
+                # print('Command: {}'.format(run_command[file_extension[1]]))
                 proc = subprocess.run([run_command[file_extension[1]], f_path], stdout=subprocess.PIPE, bufsize=0)
-                if proc.returncode is not 0:
+                # change output to string
+                proc_str = proc.stdout.decode('utf-8').rstrip("\n")
+                # Make sure the out meets the required string format
+                if check_info(output, proc_str) == '' and check_info(email, proc_str) == '':
                     print('Error in User script')
-                    error_file.append(f_path)
+                    error_files.append(f_path)
+                    # setup the dict result
+                    json_data.append(OrderedDict(
+                        {
+                            'file': f,
+                            'output': proc.stdout.decode('utf-8').rstrip("\n"),
+                            'email': check_info(email, proc_str),
+                            'fullname': check_info(names, proc_str),
+                            'HNGId': check_info(hng_id, proc_str),
+                            'language': check_info(language, proc_str),
+                            'status': 'Fail'
+                        }
+                    ))
                 else:
-                    print('Output: {}'.format(proc.stdout.decode('utf-8')))
-            print('====================================')
+                    print('Output: {}'.format(proc.stdout.decode('utf-8')), flush=True)
+                    # setup the dict result
+                    json_data.append(OrderedDict(
+                        {
+                            'file': f,
+                            'output': proc.stdout.decode('utf-8').rstrip("\n"),
+                            'email': check_info(email, proc_str),
+                            'fullname': check_info(names, proc_str),
+                            'HNGId': check_info(hng_id, proc_str),
+                            'language': check_info(language, proc_str),
+                            'status': 'Pass'
+                        }
+                    ))
+            # TODO remove these line
+            # print('====================================')
+        return json_data
     except TypeError as t_err:
         print('Error: {}'.format(t_err))
     except ValueError as v_err:
@@ -53,9 +136,15 @@ if __name__ == '__main__':
     except Exception as e_err:
         print('Error Type: {}, msg: {}'.format(type(e_err), e_err))
     finally:
-        print('Error files: {}'.format(error_file))
+        # print(for i in json_data)
+        print('json_data items: {}'.format(json.dumps(json_data, indent=4)))
+        print('Error files: {}'.format(error_files))
         print('Total files count: {}'.format(len(files)))
-        print('Files processed: {}'.format(len(files) - len(error_file)))
-        print('Files not processed: {}'.format(len(error_file)))
+        print('Files processed: {}'.format(len(files) - len(error_files)))
+        print('Files not processed: {}'.format(len(error_files)))
         print('Scripts count: {}'.format(lang_count['.js']), 'Java count: {}'.format(lang_count['.java']),
               'Php count: {}'.format(lang_count['.php']), 'Python count: {}'.format(lang_count['.py']))
+
+
+if __name__ == '__main__':
+    process_users()
---
+++ app/user_test/test_user_info.py
@@ -0,0 +1,92 @@
+import unittest
+from schema import Schema, And, Use, Or, Regex
+import re
+from ..index import check_info, process_users
+import app
+
+email_test = "Hello World, this is Idowu Toluwani with HNGi7 ID HNG-04337 using CSharp for stage 2 task and email toluwanieaidowu@gmail.com ."
+name_fail = "Hello World, this is with HNGi7 ID HNG-050709 using PHP for stage 2 task and amusaabiodun88@gmail.com"
+fail_sample = "Hello World, this is Fagoroye George-Ayomide with HNGi7 ID HNG-05#@4 using python for stage 2 task and fagoroyeayomidegmail.com"
+sample = "Hello World, this is Oke George-Kehinde with HNGi7 ID HNG-05678 using Javascript for stage 2 task and okekehinde@gmail.com"
+email = "^.+and\s+([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)[\.|\s]*$"
+names = "^Hello\s*World,\s.+is\s*([\w\s-]*)\swith.*"
+hng_id = "^Hello\s*World,\s.+ID\s(HNG-\d{5})"
+language = "^Hello\s*World,\s.+using\s(\w+)"
+main_output = "^Hello\sWorld,\s+this\s+is\s+[\w\s-]+with\s+HNGi7\s+ID\s+HNG-\d{5}\s+using\s+\w+\s+for\s+stage\s+2\s+task\s+and\s+[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[\.|\s]*$"
+main = "Hello World, this is Idowu Toluwani with HNGi7 ID HNG-04337 using CSharp for stage 2 task and email toluwanieaidowu@gmail.com ."
+main2 = "Hello World, this is Onwuka Chidumga with HNGi7 ID HNG-06529 using javascript for stage 2 task and my email is kelvinonwuka8@gmail.com\n"
+
+run_command = {
+    '.js': 'node',
+    '.py': 'python3',
+    '.php': 'php',
+    '.java': 'java'
+}
+schema = Schema([{
+    'file': str,
+    'output': lambda n: re.match(
+        r'^Hello\sWorld,\s+this\s+is\s+[\w\s-]+with\s+HNGi7\s+ID\s+HNG-\d{5}\s+using\s+\w+\s+for\s+stage\s+2\s+task\s+and\s+[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+[\.|\s]*$',
+        n) or n == '',
+    'email': lambda n: re.match(r'^.+and\s+([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)[\.|\s]*$', n) or n == '',
+    'fullname': str,
+    'HNGId': lambda n: re.match(r'HNG-\d{5}', n) or n == '',
+    'language': str,
+    'status': lambda n: n == 'Pass' or n == 'Fail'
+}])
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_process_users(self):
+        ls = process_users()
+        validated = schema.validate(ls[:100])
+        self.assertIsInstance(ls, list)
+        self.assertIsInstance(ls[0], dict)
+        self.assertTrue(validated)
+
+    def test_main_pass(self):
+        result = check_info(main_output, sample)
+        self.assertEqual(result, sample)
+
+    def test_main_fail(self):
+        result = check_info(main_output, main)
+        self.assertEqual(result, '')
+
+    def test_email(self):
+        result = check_info(email, sample)
+        self.assertEqual(result, 'okekehinde@gmail.com')
+
+    def test_names(self):
+        result = check_info(names, sample)
+        self.assertEqual(result, 'Oke George-Kehinde')
+
+    def test_hng_id(self):
+        result = check_info(hng_id, sample)
+        self.assertEqual(result, 'HNG-05678')
+
+    def test_language(self):
+        result = check_info(language, sample)
+        self.assertEqual(result, 'Javascript')
+
+    def test_email_fail(self):
+        result = check_info(email, fail_sample)
+        self.assertEqual(result, '')
+
+    def test_hng_id_fail(self):
+        result = check_info(hng_id, fail_sample)
+        self.assertEqual(result, '')
+
+    def test_name_fail(self):
+        result = check_info(names, name_fail)
+        self.assertEqual(result, '')
+
+    def test_another_email_fail(self):
+        result = check_info(email, email_test)
+        self.assertEqual(result, '')
+
+
+if __name__ == '__main__':
+    unittest.main()
